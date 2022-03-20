c34bf06155014f00fe41a3ec9d1deae36d74507b
prajavalpatki <prajavalpatki.com>
2022-03-20 17:08:08 +0530

    Greater ro less than the other line

--- LineComparison.java
+++ LineComparison.java
@@ -10,6 +10,7 @@ public class LineComparison {
         System.out.println("Line 01 :- enter X1 and Y1 value");
         int line1X1 = in.nextInt();
         int line1Y1 = in.nextInt();
+
         System.out.println("Line 01 :- enter X2 and Y2 value");
         int line1X2 = in.nextInt();
         int line1Y2 = in.nextInt();
@@ -21,19 +22,25 @@ public class LineComparison {
         System.out.println("Line 02 :- enter X1 and Y1 value"); //enter value
         int line2X1 = in.nextInt();
         int line2Y1 = in.nextInt();
+
         System.out.println("Line 02 :- enter X2 and Y2 value");
         int line2X2 = in.nextInt();
         int line2Y2 = in.nextInt();
 
         int line2 = (int)Math.sqrt( (line2X2 - line2X1) ^ 2 + (line2Y2 - line2Y1) ^ 2 );
-        System.out.println("Line One Length Is:- " + line1);
+        System.out.println("Line two Length Is:- " + line2);
 
 
         if (line1 == line2){
             System.out.println("Line 01 And Line 02 Is Equal");
         }
-        else {
-            System.out.println("Line 01 And Line 02 Is Not Equal");
+        else if(line1 > line2){
+            System.out.println("Line 01 Is Greater Than Line 02");
         }
+
+        else {
+            System.out.println("Line 01 Is Less Than Line 02");
+        }
+
     }
 }
